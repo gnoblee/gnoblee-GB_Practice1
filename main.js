cbf84eeb22ec66b4679ed0ef1bf664260bca45ea
gnoblee <gnoblee@gmail.com>
2020-05-24 23:11:03 +0900

    자바스크립트 설정

---
+++ main.js
@@ -0,0 +1,20 @@
+var index = 0;
+var colors = [ "red", "orange", "yellow", "green", "blue", "Indigo", "purple"];
+var max = colors.length;
+var timerId;
+
+function start(){
+    timerId = setInterval(changeColor, 1000);
+}
+
+function changeColor(){     
+        if( index != max){
+            console.log( index);
+            document.body.style.backgroundColor = colors[ index];           
+            index++;
+        }
+        else{           
+            clearInterval( timerId);
+            alert("배경색 변경완료");
+        }
+}
