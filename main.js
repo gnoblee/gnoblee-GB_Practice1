07d0a1bd755e4cb700d73aac1daccefb32d8fbee
gnoblee <gnoblee@gmail.com>
2020-05-25 01:43:47 +0900

    수정

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var colors = [ "red", "orange", "yellow", "green", "blue", "navy", "purple"];
 var max = colors.length;
 var timerId;
 
-function start(){
+function start2(){
     timerId = setInterval(changeColor, 1000);
 }
 
 function changeColor(){     
         if( index != max){
-            console.log( index);
-            document.body.style.backgroundColor = colors[ index];           
+            console.log(index);
+            document.body.style.backgroundColor = colors[index];           
             index++;
         }
         else{           
@@ -18,3 +18,25 @@ function changeColor(){
             alert("배경색 변경완료");
         }
 }
+
+var index = 0;
+var color = ["gray"];
+var max = colors.length;
+var timerId;
+
+function start1(){
+    timerId = setInterval(changeColor, 50);
+}
+
+function changeColor(){     
+        if( index != max){
+            console.log(index);
+            document.body.style.backgroundColor = color[index];           
+            index++;
+        }
+        else{           
+            clearInterval( timerId);
+            alert("배경색 변경완료");
+        }
+}
+           
